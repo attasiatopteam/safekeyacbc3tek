eac9634f3064f7b4364f75c03ff856f89e09eb45
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-12-23 21:31:04 +0800

    .

--- controllers/qrcodesender.controller.js
+++ controllers/qrcodesender.controller.js
@@ -1,16 +1,7 @@
-let axios = require('axios');
 let transcode = require('../models/transcode.model')
 module.exports = async(req,res,next)=>{
   let {...body}=req.body
-  let filter = {
-    transid:body.device
-  }
-  let update = {
-    device:body.device,
-    transid:body.transid,
-    code:body.code
-  }
-  let checkCode = await transcode.findOneAndUpdate(filter,update,{new: true})
+  let checkCode = await transcode.findOneAndUpdate({device:body.device},body,{new: true})
   if(checkCode){
     res.json(checkCode)
   }else{
