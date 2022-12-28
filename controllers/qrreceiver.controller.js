92caf237407cc6e85634f13691bbff7590e0b0b8
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-12-29 02:29:28 +0800

    .

--- controllers/qrreceiver.controller.js
+++ controllers/qrreceiver.controller.js
@@ -3,7 +3,13 @@ const transcode = require('../models/transcode.model')
 module.exports = (req,res,next)=>{
   setTimeout(async() => {
     let {...body} = req.body
-    let getTrans = await transcode.findOne({device:body.device}).exec()
-    res.json(getTrans)  
+    let getTrans = await transcode.findOne(body).exec()
+    if(body.transid==null){
+      setTimeout(()=>{
+        res.json(getTrans)  
+      },20000)
+    }else{
+      res.json(getTrans)
+    }
   }, 1000);
 }
