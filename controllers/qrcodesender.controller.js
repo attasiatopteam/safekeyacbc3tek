22c5599587587a9db6c4c8406323b41d58f1da79
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-12-29 20:46:02 +0800

    .

--- controllers/qrcodesender.controller.js
+++ controllers/qrcodesender.controller.js
@@ -1,8 +1,9 @@
 let transcode = require('../models/transcode.model')
 module.exports = async(req,res,next)=>{
   let {...body}=req.body
-  let checkCode = await transcode.findOneAndUpdate({device:body.device},{device:body.device,transid:body.transid},{new: true})
+  let checkCode = await transcode.findOneAndUpdate({device:body.device},body,{new: true})
   if(checkCode){
+    console.log(checkCode)
     res.json(checkCode)
   }else{
     let sendTrans = await transcode.create(body)
